084399c17eb76f811dcf8f072effe92daf17fad8
Najmi Irfan <irfanizhak@gmail.com>
2025-05-30 12:10:44 +0800

    setup a context to switch pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import {
+  createContext,
+  useState,
+  type Dispatch,
+  type SetStateAction
+} from 'react'
 import { Landing } from './pages/Landing'
+import { Game } from './pages/Game'
+import { Scores } from './pages/Scores'
+
+type PageContextType = {
+  page: number
+  setPage: Dispatch<SetStateAction<number>>
+}
+
+export const PageContext = createContext<PageContextType | null>(null)
 
 function App () {
+  const [page, setPage] = useState<number>(0)
+
   return (
-    <>
-      <Landing />
-    </>
+    <PageContext.Provider value={{ page, setPage }}>
+      <>
+        {page === 0 && <Landing />}
+        {page === 1 && <Game />}
+        {page === 2 && <Scores />}
+      </>
+    </PageContext.Provider>
   )
 }
 
