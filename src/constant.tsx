e1b9a75109f67a2464f619be124e49994b9f5196
Najmi Irfan <irfanizhak@gmail.com>
2025-05-30 15:11:15 +0800

    fix up styling for landing page

---
+++ src/constant.tsx
@@ -0,0 +1,7 @@
+// time for per round
+export const GAME_DURATION = 20 
+
+// colour list for target colour and coloured circle
+export const  COLOUR_LIST = [ "red", "blue", "yellow", "pink", "grey", "purple", "green", "cyan"]
+
+export const CIRCLE_SIZE = 100 // Size of every circle (in px)
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,33 +1,42 @@
 export function Landing () {
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-100 px-4'>
-      <div className='bg-white shadow-2xl rounded-2xl overflow-hidden flex flex-col md:flex-row w-full max-w-4xl'>
+    <div className='min-h-screen flex items-center justify-center px-4'>
+      <div className='bg-[#2A2A29] shadow-2xl rounded-md overflow-hidden flex flex-col md:flex-row w-full max-w-4xl'>
         {/* Left: Login Form */}
         <div className='md:w-1/2 p-8 flex flex-col justify-center'>
-          <h2 className='text-3xl font-bold mb-4 text-gray-800'>React Game</h2>
-          <div>
-            <label className='block mb-1 text-sm font-medium text-gray-600'>
-              Player Name
+          <div className='text-white text-center'>
+            <h2 className='text-3xl font-bold mb-4'>Reaction Game</h2>
+            <p>How to play:</p>
+            <p>
+              Select the colored circle which which stated as target color (20
+              seconds per round)
+            </p>
+          </div>
+          <div className='relative w-full my-4'>
+            <label
+              htmlFor='playerName'
+              className='absolute left-3 top-1 text-xs text-white font-bold px-1 z-10'
+            >
+              Player Name <span className="text-red-400">*</span>
             </label>
             <input
-              type='password'
-              className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-              placeholder='********'
+              type='text'
+              id='playerName'
+              className='w-full px-4 pt-6 pb-2 text-sm border border-white rounded-xl bg-transparent'
             />
           </div>
-          <button
-            type='submit'
-            className='w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-200'
-          >
+
+          <button type='submit' className='w-full bg-white py-2 rounded-md'>
             Start Game
           </button>
         </div>
 
         {/* Right: Notes/Info */}
-        <div className='md:w-1/2 bg-blue-100 p-8 flex items-center justify-center'>
-          <p className='text-blue-800 text-center text-lg font-medium'>
+        <div className='md:w-1/2 bg-[#fa4454] p-8 flex flex-col'>
+          <p className='text-black text-center text-3xl font-bold pb-4'>
             Top scores
           </p>
+          <p>No top score available right now</p>
         </div>
       </div>
     </div>
